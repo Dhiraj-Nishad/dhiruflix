47d0bc2443763fea798130ec817d39e62a780d8f
Dhiraj Nishad <70431995+Dhiraj-Nishad@users.noreply.github.com>
2025-04-22 15:31:04 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,8 +9,9 @@ const router = require("./routes/Users.js")
 
 
 const app = express()
-app.set("view engine", "ejs")
-app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
